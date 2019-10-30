ad67f640c3bccdb01f2fdccc995b9cf84c41ab8f
Jared Chapman <thejaredchapman@gmail.com>
2019-10-29 20:55:21 -0500

    Create facebook.js

---
+++ facebook.js
@@ -0,0 +1,34 @@
+# CREATE A FACEBOOK 
+
+# enter username
+{
+	username: "Jared",
+	password: "cooldad"
+
+}
+
+# username in system
+var database = [
+	{
+		username: "Jared",
+		password: "cooldad"
+	}
+];
+
+#
+
+var newsfeed = [
+	{
+		usernname: "Avery",
+		timeline: "I love Jared so much!"
+	},
+	{
+		username: "Adam",
+		timeline: "Surfing on cows"
+	},
+	{
+		username: "Bartholemule"
+		timeline: "Me and Betsy went for a light jog, lovin' popsicles, yum yum yum!!"
+	}
+
+]
